4dcfe64ff0909600fd37734e336a3c7ed366a813
Débora Christyna <cdias.proj@gmail.com>
2022-07-27 00:16:44 -0300

    fix bug rotas e port

--- src/routes/usuarioRotas.js
+++ src/routes/usuarioRotas.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controller/usuarioController')
 
-router.post('/usuarios/criar', controller.create)
 router.get('/usuarios/', controller.getAll)
+router.post('/usuarios/criar', controller.create)
 router.delete('/usuarios/:id', controller.deleteById)
 router.post('/usuarios/login', controller.login)
 
